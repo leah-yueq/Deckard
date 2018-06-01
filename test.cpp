8acffa24b78d6d9c01e5df20547c1f6400e5dca4
yueq <Y.Qiao@fr.merce.mee.com>
2018-06-01 17:44:47 +0200

    test for git

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: /home/yue/Documents/Deckard/test.cpp
+	> Author: Y.Qiao
+	> Mail: Y.Qiao@fr.merce.mee.com 
+	> Created Time: ven. 01 juin 2018 11:40:09 CEST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hello world"<<endl;
+	return 0;
+}
